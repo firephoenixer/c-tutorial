4e9fc8893daaff4fee7dbefae9d1c4ec798028d9
Billion Leo <owen00721@gmail.com>
2022-10-30 10:15:23 +0800

    understand static variable

--- static.c
+++ static.c
@@ -4,7 +4,7 @@ void foo()
 {
     int a = 10;
     static int sa = 10;  // seems can remmber the value for next call
-
+                         // seems can only assign constant value not the same in c++
     a += 5;
     sa += 5;
     // show the difference 
@@ -15,9 +15,10 @@ void foo()
 int main()
 {
     int i;
-
     for (i = 0; i < 10; ++i)
         foo();
+
+    return 0;
 }
 
 
