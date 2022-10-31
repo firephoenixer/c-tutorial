bd47ff495f1e912bc94000de8139876853592eba
Billion Leo <owen00721@gmail.com>
2022-10-31 11:43:07 +0800

    I tpyed some message in vs code this time.

--- static.c
+++ static.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+// use "size a.exe" can see the execuble file's size in different segment of the RAM.
+static int iTest, iTest2;
+static int iTest3 = 1;
+
+
+static int iTest5;       // seems in bss segment
+static int iTest5 = 31;  // seems move to data segment when get a !0 value
+static int iTest5;       // wo should aviod to do this, we test this to understand programme well.
+                         // seems still in data segment with the value 31
+// iTest5 = 50;  // Not allowed in globle scope!
 
 void foo()
 {
@@ -15,9 +25,19 @@ void foo()
 int main()
 {
     int i;
-    for (i = 0; i < 10; ++i)
+    int iTest4;
+    iTest4 = 2;
+    printf("Address of iTest5: %x\n", &iTest5);
+    static int iTest5;   // seems new static variable in bss segment
+    printf("Address of iTest5: %x\n", &iTest5);
+    for (i = 0; i < 3; ++i)
         foo();
-
+    printf("%d%d\n", iTest,iTest2);
+    printf("%d\n", iTest3);
+    printf("%d\n", iTest5);    // 31 or 0? 0, Beacause iTest5 is a new bss segment variable.
+    iTest5 = 50;
+    printf("Address of iTest5: %x\n", &iTest5);
+    printf("%d\n", iTest5);    // where is it? if move to data segment, will there has a conflict?
     return 0;
 }
 
