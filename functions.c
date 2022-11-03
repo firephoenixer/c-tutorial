97b43bb0e4103f76a63e10bdd26088b44913dba4
Billion Leo <owen00721@gmail.com>
2022-11-03 08:48:06 +0800

    About function

---
+++ functions.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+// Before using, functions are needly declared.
+int g_sum(int, int);
+
+int main(){
+
+    printf("sum of 3+4=%d\n", g_sum(3,4));
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
