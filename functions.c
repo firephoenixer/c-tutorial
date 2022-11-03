3e94476c34635db48f07d9357594d232968f8f8a
Billion Leo <owen00721@gmail.com>
2022-11-03 09:08:34 +0800

    Edited

--- functions.c
+++ functions.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 // Before using, functions are needly declared.
 int g_sum(int, int);
+// gcc .\functions.c .\globalFunctions.c  
+// Notice: You must compile the associated file too, or you will lost the definity of the function!!!
 
 int main(){
 
