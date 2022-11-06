a1eadaf0fa6da0e820de8abc47671e7b1b375c57
Billion Leo <owen00721@gmail.com>
2022-11-06 10:35:22 +0800

    Function pointer

---
+++ functionPointer.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+// This is a C example for function pointer.
+float sum(float a, float b) {return a+b;}
+float min(float a, float b) {return a-b;}
+float mul(float a, float b) {return a*b;}
+float dev(float a, float b) {return a/b;}
+
+int main(){
+    // ordinary call
+    printf("%f\n", sum(4,2));
+    printf("%f\n", min(4,2));
+    printf("%f\n", mul(4,2));
+    printf("%f\n", dev(4,2));
+    printf("\n");
+
+    // a function pointer
+    float (*pSum) (float, float) = sum;  // notice: we use (*pSum) make sure pSum a pionter, otherwise pSum will 
+                                         // associate with (float, float) witch makes no sense!!!
+    printf("%f\n", pSum(4,2));
+    printf("\n");
+
+    // When function pointers useful
+    float (*pFuns[4]) (float, float) = {sum, min, mul, dev};  // (*pFuns[4]) means array content pointers
+    for(int i = 0; i < 4; i++)  printf("%f\n", pFuns[i](4, 2));
+
+    // function pointers make programme more flexible
+
+
+    return 0;
+
+}
