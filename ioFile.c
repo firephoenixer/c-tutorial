13f895e9a86dde9c85da6951415cd666edbc6445
Billion Leo <owen00721@gmail.com>
2022-11-08 17:09:27 +0800

    file operation

--- ioFile.c
+++ ioFile.c
@@ -1,3 +1,36 @@
+#include <stdio.h>
+// This is a C example for files.
+
+int main(){
+
+    FILE *fTxt;
+    /* Mode：r   --  for reading only, must existing
+             w   --  for writing, if not existing then create. start writing from the beginning
+             a   --  for appending, if not existing then create.
+             r+  --  for reading and writing
+             w+  --  for reading and writing, if existing delete the content, or create new file
+             a+  --  for reading and writing, if existing read at the beginning but only can writing for 
+                     appending, or create new file
+    for binary files mode: rb, wb, ab, rb+, r+b, wb+, w+b, ab+, a+b
+    */
+
+    fTxt = fopen("/src/test.txt", "w+");
+    // These two functions actually the same.
+    fprintf(fTxt, "This line1.\n");
+    fputs("This is line2.\n", fTxt);
+
+    fclose(fTxt);
+
+
+    printf("Hello world!");
+    return 0;
+
+}
+
+
+
+
+
 
 
 
