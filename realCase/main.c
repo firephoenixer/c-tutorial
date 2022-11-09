4bf08d33d44bc8023fd92a76eb8de9c9efd7136c
Billion Leo <owen00721@gmail.com>
2022-11-09 21:07:51 +0800

    Modified at 21:08 09/11/2022

---
+++ realCase/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+// There are two poker players. Caculate the rate of winning when they allin preflop.
+
+int main(){
+
+    // set two players' hands
+    char p[2][2][2] = {0};
+
+    printf("player 1's first card: %d%d, second card: %d%d\n", p[0][0][0], p[0][0][1],p[0][1][0],p[0][1][1]);
+    printf("player 2's first card: %d%d, second card: %d%d\n", p[1][0][0], p[1][0][1],p[1][1][0],p[1][1][1]);
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
