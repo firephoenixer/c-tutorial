108373639c93f20a8ccab918bcfa16d870fd4269
Billion Leo <owen00721@gmail.com>
2022-11-10 17:19:12 +0800

    e

--- realCase/main.c
+++ realCase/main.c
@@ -6,10 +6,12 @@ int main(){
 
     // set two players' hands
     char p[2][2][2] = {0};
-    show_hands(p, 2);
+    show_hands(p, 2);  // 2 means the number of players
     
     // use 2-13 present 2-9-T-J-Q-K, 14 present A, 0:club, 1:diamond, 2:heart, 3:spade
     // use hands.txt store witch hands we are interesting about. format as: A0A1|92T3  one line one hands
+
+
 
 
 
