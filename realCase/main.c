9f25fe386326f820206a0d389fa92b2d03bfe647
Billion Leo <owen00721@gmail.com>
2022-11-10 16:17:49 +0800

    m

--- realCase/main.c
+++ realCase/main.c
@@ -5,7 +5,8 @@ int main(){
 
     // set two players' hands
     char p[2][2][2] = {0};
-
+    
+    //*** move this code to an indepence support file 11-09
     printf("player 1's first card: %d%d, second card: %d%d\n", p[0][0][0], p[0][0][1],p[0][1][0],p[0][1][1]);
     printf("player 2's first card: %d%d, second card: %d%d\n", p[1][0][0], p[1][0][1],p[1][1][0],p[1][1][1]);
     return 0;
