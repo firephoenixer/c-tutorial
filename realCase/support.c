e6f6d6a9d82932ba8d6846bd2649c55bc40eeb18
Billion Leo <owen00721@gmail.com>
2022-11-10 18:40:54 +0800

    E

--- realCase/support.c
+++ realCase/support.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-extern int errno;  // use errno in errno.h
 
 // In this file, store support fanctions for the main programme
 
@@ -38,24 +37,15 @@ unsigned int read_hands(char *pHeal, char *pStrFile)
     fTxt = fopen(pStrFile, "r");
 
     // check if opened success
-    int errnum;
+    // int errnum;
     if (fTxt == NULL) {
-      errnum = errno;
-      // use stderr file stream to Output all the errors.
-      fprintf(stderr, "Value of errno: %d\n", errno);
-      // Show the error text.
       perror("Error printed by perror");
-      // using strerror() to show errno present what meaning
-      fprintf(stderr, "Error opening file: %s\n", strerror( errnum ));
       return 0;
    }else{
-      printf("File address: %p\n", fTxt);
+      printf("File hands.txt address: %p\n", fTxt);
    }
 
-    fprintf(fTxt, "This is line01.\n");
-    fputs("This is line01.\n", fTxt);
-
-    char buff[10] = {0};
+    char buff[10] = {0};  // to store a hand temporily
     // get hands circually
     fgets(buff, 10, fTxt);  // seems stop when seen \n, inclued \n
     printf("1 : %s\n", buff);
