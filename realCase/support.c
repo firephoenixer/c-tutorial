5e3b84b485f0910b9a5ff1e26133a83e17d4329f
Billion Leo <owen00721@gmail.com>
2022-11-10 18:20:36 +0800

    E

--- realCase/support.c
+++ realCase/support.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+
+extern int errno;  // use errno in errno.h
 
 // In this file, store support fanctions for the main programme
 
@@ -28,9 +33,35 @@ void check_allo(void *pAdd)
 // read hands from hands.txt to a heal memory, return hands number copyed.
 unsigned int read_hands(char *pHeal, char *pStrFile)
 {
-    unsigned int n = 0;
+    unsigned int n = 0;  // present how many hands readed
+    FILE *fTxt;
+    fTxt = fopen(pStrFile, "r");
+
+    // check if opened success
+    int errnum;
+    if (fTxt == NULL) {
+      errnum = errno;
+      // use stderr file stream to Output all the errors.
+      fprintf(stderr, "Value of errno: %d\n", errno);
+      // Show the error text.
+      perror("Error printed by perror");
+      // using strerror() to show errno present what meaning
+      fprintf(stderr, "Error opening file: %s\n", strerror( errnum ));
+      return 0;
+   }else{
+      printf("File address: %p\n", fTxt);
+   }
+
+    fprintf(fTxt, "This is line01.\n");
+    fputs("This is line01.\n", fTxt);
+
+    char buff[10] = {0};
+    // get hands circually
+    fgets(buff, 10, fTxt);  // seems stop when seen \n, inclued \n
+    printf("1 : %s\n", buff);
 
 
+    fclose(fTxt);
     printf("Hands read success: %d", n);
     return n;
 }
