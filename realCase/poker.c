fca97c9617dbf985747c803fefbcaa25ea8e8eae
Billion Leo <owen00721@gmail.com>
2022-11-11 19:32:15 +0800

    E

---
+++ realCase/poker.c
@@ -0,0 +1,32 @@
+// show a hand's (7 cards) strength
+
+#include <stdio.h>
+
+
+char *calutate_strength(char hand[2][2], char puCards[5][2])
+{
+    char *sth[7][5] = {0};
+
+
+    
+    return sth;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
