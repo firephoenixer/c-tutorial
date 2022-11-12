891b9e0f24348c717a32b1a0fc4323909fffe4a2
Billion Leo <owen00721@gmail.com>
2022-11-12 10:57:46 +0800

    E

---
+++ realCase/core.c
@@ -0,0 +1,32 @@
+// to caculate the rate of winning for each hand
+#include <stdio.h>
+
+// create a circluar, in each possiblity of public cards which hand will win
+void caculate_rate(char aHand[2][2][2])
+{
+
+
+
+
+
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
